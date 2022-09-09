84a9a9098e21de5fd1373b6c1ae93aa5ec05a74f
Andrew <balshowandrew@gmail.com>
2022-09-09 22:43:57 +0300

    add new templates

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('books/', book_list, name='book_list')
-]
-
+    path('books/', book_list, name='book_list'),
+    path('books/<slug:content_slug>/', show_content, name='content'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    ]
--- books/views.py
+++ books/views.py
@@ -3,22 +3,52 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
-menu = [{'title': 'О сайте', 'url_name': 'about'},
-        {'title': 'Обратная связь', 'url_name': 'contact'},
-        {'title': 'Войти', 'url_name': 'login'}
+menu = [
+    {'title': "О сайте", 'url_name': 'home'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'}
 ]
 
-def index(request):
-    books = Books.objects.all()
 
+def index(request):
     context = {
-        'books': books,
         'menu': menu,
         'title': "Главная страница",
-        'cat_selected': 0,
     }
     return render(request, 'books/index.html', context=context)
 
 
 def book_list(request):
-    return HttpResponse('Библиотека')
+    books = Books.objects.all()
+
+    context = {
+        'books': books,
+        'menu': menu,
+        'title': 'Список книг',
+    }
+    return render(request, 'books/book_list.html', context=context)
+
+
+def show_content(request, content_slug):
+    context = {
+        'menu': menu,
+        'title': 'Статья'
+    }
+    return render(request, 'books/show_content.html', context=context)
+
+
+
+def contact(request):
+     context = {
+         'menu': menu,
+         'title': 'Обратная связь',
+     }
+     return render(request, 'books/contact.html', context=context)
+
+
+def login(request):
+    context = {
+        'menu': menu,
+        'title': 'Вход',
+    }
+    return render(request, 'books/login.html', context=context)
